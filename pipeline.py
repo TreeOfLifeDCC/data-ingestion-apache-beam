3e46cd88e0c385c2b7f1dfb36154673d75c1fbe4
Alexey Sokolov <alexey@ebi.ac.uk>
2024-05-03 09:34:40 +0100

    Providing absolute path

--- pipeline.py
+++ pipeline.py
@@ -35,7 +35,7 @@ options.view_as(WorkerOptions).max_num_workers = 32
 options.view_as(WorkerOptions).disk_size_gb = int(opts.disk_size_gb)
 options.view_as(WorkerOptions).machine_type = opts.machine_type
 options.view_as(SetupOptions).save_main_session = True
-options.view_as(SetupOptions).setup_file = 'setup.py'
+options.view_as(SetupOptions).setup_file = '/home/alexey/data-ingestion-apache-beam/setup.py'
 options.view_as(GoogleCloudOptions).job_name = '{0}{1}'.format('my-pipeline-', time.time_ns())
 options.view_as(StandardOptions).runner = opts.runner
 
