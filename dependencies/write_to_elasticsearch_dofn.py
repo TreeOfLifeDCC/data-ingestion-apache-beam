7e9a2bf956e616a197c491181b085e6f4be19f8f
Juan Pablo Narvaez Gomez <juann@c02f37e5ml7h.windows.ebi.ac.uk>
2024-06-27 15:24:01 +0100

    Example of usage added.

--- dependencies/write_to_elasticsearch_dofn.py
+++ dependencies/write_to_elasticsearch_dofn.py
@@ -1,6 +1,61 @@
 # Inspired by: https://github.com/rrmerugu-archive/apache-beam-io-extras/blob/master/beam_io_extras/elasticsearch.py#L92
 # and: https://github.com/apache/beam/blob/master/sdks/python/apache_beam/io/mongodbio.py#L770
 
+"""This class writes JSON data to Elasticsearch
+
+Example of usage::
+
+pipeline | 'WriteToElastic' >> beam.ParDo(WriteToElasticsearchDoFn(
+                host=opts.address,
+                username=opts.username,
+                password=opts.password,
+                index=opts.index
+            )
+         )
+     
+Ensure that you have the following command line arguments set in your script to connect to Elasticsearch:
+address, username, password, and index.
+
+CLI call: python script.py --address=<address> --username=<username> --password=<password> --index='test'
+
+Example script.py:
+___________
+
+import argparse
+import apache_beam as beam
+from write_to_elastic_dofn import WriteToElasticsearchDoFn
+
+parser = argparse.ArgumentParser(description='Load DToL data from string into Elasticsearch')
+parser.add_argument('--address', required=True, help='Elasticsearch URL')
+parser.add_argument('--username', required=True, help='Elasticsearch username')
+parser.add_argument('--password', required=True, help='Elasticsearch password')
+parser.add_argument('--index', required=True, help='Elasticsearch index name')
+
+opts = parser.parse_args()
+
+data = [
+    {
+        'field1': 'string1',
+        'field2': 'string2'
+    }
+]
+
+with beam.Pipeline() as p:
+    (p
+     | 'ReadStrings' >> beam.Create(data)
+     | 'WriteToElastic' >> beam.ParDo(WriteToElasticsearchDoFn(
+                host=opts.address,
+                username=opts.username,
+                password=opts.password,
+                index=opts.index
+            )
+         )
+     )
+___________
+
+"""
+
+
 import apache_beam as beam
 from elasticsearch import Elasticsearch, helpers
 from datetime import datetime
