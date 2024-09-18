75df3e0f71a9ad5ca98aca95ca7b08732c09b975
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2024-09-18 14:50:13 +0100

    Draft of DAG with dummy apache beam pipeline.

--- annotations_airflow_dag.py
+++ annotations_airflow_dag.py
@@ -17,28 +17,10 @@ from lxml import etree
 from google.cloud import storage
 from urllib.request import urlretrieve
 
-# # Command line arguments
-# parser = argparse.ArgumentParser(description='ETL DAG for annotation data: from Ensembl into BigQuery')
-# parser.add_argument('--project', required=True, help='Specify Google Cloud project')
-# parser.add_argument('--bq_dataset_name', required=True, help='BigQuery dataset name')
-# parser.add_argument('--region', required=True, help='Specify Google Cloud region')
-# parser.add_argument('--stagingLocation', required=True, help='Specify Cloud Storage bucket for staging')
-# parser.add_argument('--tempLocation', required=True, help='Specify Cloud Storage bucket for temp')
-# parser.add_argument('--runner', required=True, help='Specify Apache Beam Runner')
-# parser.add_argument('--disk_size_gb', required=True, help='Disk Size of Workers')
-# parser.add_argument('--machine_type', required=True, help='Machine Type of Worker')
-# parser.add_argument('--max_num_workers', required=True, help='Maximum number of workers')
-# parser.add_argument('--save_main_session', required=True, help='Save main session')
-# parser.add_argument('--setup_file', required=True, help='Setup file')
-# parser.add_argument('--annotations_url', required=True, help="Website with links for annotation data: "
-#                                                              "e.g. 'https://projects.ensembl.org/darwin-tree-of-life/'")
-# parser.add_argument('--annotation_json_file', required=True, help='Name of json file to store annotation links')
-# parser.add_argument('--taxonomy_jsonl_file', required=True, help='Name of jsonl file to store taxonomy data')
-# parser.add_argument('--apache_beam_script', required=True, help='Name of python script to run on Apache Beam')
-# opts = parser.parse_args()
-
+# DAG set up variables
+GCP_REGION = os.environ.get('GCP_REGION', 'europe-west-2')
 GCP_PROJECT = os.environ.get('GCP_PROJECT', 'prj-ext-prod-planet-bio-dr')
-GCP_BQ_DATASET = os.environ.get('GCP_BQ_DATASET', 'annotations')
+SETUP_FILE = os.environ.get('SETUP_FILE')
 GCP_STAGING_LOCATION = os.environ.get('GCP_STAGING_LOCATION')
 GCP_TEMP_LOCATION = os.environ.get('GCP_TEMP_LOCATION')
 GCP_RUNNER = os.environ.get('GCP_RUNNER')
@@ -46,21 +28,20 @@ GCP_DISK_SIZE = os.environ.get('GCP_DISK_SIZE')
 GCP_MACHINE_TYPE = os.environ.get('GCP_MACHINE_TYPE')
 GCP_MAX_WORKERS = os.environ.get('GCP_MAX_WORKERS')
 GCP_SAVE_MAIN_SESSION = os.environ.get('GCP_SAVE_MAIN_SESSION')
-SETUP_FILE = os.environ.get('SETUP_FILE')
+GCP_BQ_DATASET = os.environ.get('GCP_BQ_DATASET', 'annotations')
+
 ANNOTATIONS_URL = os.environ.get('ANNOTATIONS_URL', 'https://projects.ensembl.org/darwin-tree-of-life/')
-ANNOTATION_JSON = os.environ.get(
-    'ANNOTATION_JSON',
-    '/Users/juann/PycharmProjects/annotationsDataIngestion/annotations_to_import.json'
-)
+ANNOTATION_JSON = os.environ.get( 'ANNOTATION_JSON', 'annotations_to_import.json')
 APACHE_BEAM_SCRIPT = os.environ.get(
     'APACHE_BEAM_SCRIPT',
-    '/Users/juann/PycharmProjects/annotationsDataIngestion/mock_beam_pipeline.py'
+    # Using dummy pipeline here. Actual pipeline not tested yet.
+    'gs://dummy_beam_pipeline.py'
 )
 
 @dag(schedule=None,
      start_date=pendulum.datetime(2024, 8, 1, tz="UTC"))
 def annotations_pipeline_dag():
-    @task()
+    @task
     def parse_annotations(url, json_file_name):
         print(f'Parsing annotations urls from {url}')
 
@@ -101,7 +82,7 @@ def annotations_pipeline_dag():
 
         return blob.name
 
-    @task()
+    @task
     def annotations_to_cloud_storage(annotations_url_blob_name):
         client = storage.Client(project=GCP_PROJECT)
         bucket = client.get_bucket(GCP_PROJECT)
@@ -177,30 +158,36 @@ def annotations_pipeline_dag():
 
         print(f'Phylogeny table saved to {blob_taxonomy.name}')
 
+# To run the actual pipeline on DataFlow (not tested)
+#     run_beam_pipeline = BeamRunPythonPipelineOperator(
+#         task_id="GCS_to_BigQuery_Beam_pipeline",
+#         py_file=APACHE_BEAM_SCRIPT,
+#         runner=GCP_RUNNER,  # "DirectRunner",  # "DataflowRunner"
+#         py_options=[],
+#         pipeline_options={
+#             'stagingLocation': GCP_STAGING_LOCATION,
+#             'tempLocation': GCP_TEMP_LOCATION,
+#             'bq_dataset_name': GCP_BQ_DATASET,
+#             'disk_size_gb': int(GCP_DISK_SIZE),
+#             'machine_type': GCP_MACHINE_TYPE,
+#             'max_num_workers': GCP_MAX_WORKERS,
+#             'save_main_session': GCP_SAVE_MAIN_SESSION,
+#             'setup_file': SETUP_FILE
+#         },
+#         py_requirements=["apache-beam[gcp]==2.26.0"],
+#         py_interpreter="python3",
+#         py_system_site_packages=False,
+#         dataflow_config=DataflowConfiguration(
+#             job_name='{0}{1}'.format('my-pipeline-', time.time_ns()),
+#             project_id=GCP_PROJECT,
+#             location=GCP_REGION
+#         )
+#     )
+
     run_beam_pipeline = BeamRunPythonPipelineOperator(
-        task_id="GCS_to_BigQuery_Beam_pipeline",
+        task_id="ingest_annotations_data_apache_beam",
         py_file=APACHE_BEAM_SCRIPT,
-        runner=GCP_RUNNER,  # "DirectRunner",  # "DataflowRunner"
-        py_options=[],
-        pipeline_options={
-            'stagingLocation': GCP_STAGING_LOCATION,
-            'tempLocation': GCP_TEMP_LOCATION,
-            'bq_dataset_name': GCP_BQ_DATASET,
-            'disk_size_gb': int(GCP_DISK_SIZE),
-            'machine_type': GCP_MACHINE_TYPE,
-            'max_num_workers': GCP_MAX_WORKERS,
-            'save_main_session': GCP_SAVE_MAIN_SESSION,
-            'setup_file': SETUP_FILE
-        },
-        py_requirements=["apache-beam[gcp]==2.26.0"],
-        py_interpreter="python3",
-        py_system_site_packages=False,
-        dataflow_config=DataflowConfiguration(
-            job_name='{0}{1}'.format('my-pipeline-', time.time_ns()),
-            project_id=GCP_PROJECT,
-            location='europe-west2'
-        )
-    )
+        runner="DirectRunner")
 
     # Defining dependencies between airflow tasks
     blob_annotations_name = parse_annotations(url=ANNOTATIONS_URL, json_file_name=ANNOTATION_JSON)
